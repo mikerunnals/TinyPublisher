3dc1c57c284b17f22914181c1781c4c1d1e35076
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-08-02 16:06:32 -0400

    WIP

--- TinyPublisher/Sources/TinyPublisher/Publisher.swift
+++ TinyPublisher/Sources/TinyPublisher/Publisher.swift
@@ -10,12 +10,52 @@ extension Publisher {
 
         return subscriber.eraseToAnyCancellable()
     }
+    
+    func assign<Root>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root) -> AnyCancellable where Self.Failure == Never {
+        let subscriber = AssignToSubscriber<Self.Output, Self.Failure>()
+        return subscriber
+    }
 }
 
-class ClosureSubscriber<Input, Failure> : Subscriber where Failure : Error {
+fileprivate class AssignToSubscriber<Input, Failure> : Subscriber where Failure : Error {
+   
+    var combineIdentifier: CombineIdentifier {
+        subscription.combineIdentifier
+    }
     
-    let combineIdentifier = CombineIdentifier()
+    typealias Input = Input
+    typealias Failure = Failure
+
+    fileprivate var subscription: Subscription!
     
+    func eraseToAnyCancellable() -> AnyCancellable {
+        let subscriptionCancellable = SubscriptionCancellable(subscriber: self, subscription: subscription)
+        return AnyCancellable(cancellable: subscriptionCancellable)
+    }
+    
+    func receive(_ input: Input) -> Subscribers.Demand {
+       return .unlimited
+    }
+
+    func receive() -> Subscribers.Demand {
+       return .unlimited
+    }
+
+    func receive(subscription: Subscription) {
+       
+    }
+
+    func receive(completion: Subscribers.Completion<Failure>) {
+       
+    }
+}
+
+fileprivate class ClosureSubscriber<Input, Failure> : Subscriber where Failure : Error {
+    
+    var combineIdentifier: CombineIdentifier {
+        subscription.combineIdentifier
+    }
+
     typealias Input = Input
     typealias Failure = Failure
         
