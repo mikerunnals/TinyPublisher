d11bc010670b202c99c05308d7b16775f3821c80
Mike Runnals <mikerunnals@users.noreply.github.com>
2020-08-03 09:13:41 -0400

    Assign To On (#6)

---
+++ TinyPublisher/Tests/TinyPublisherTests/PublisherAssignTests.swift
@@ -0,0 +1,73 @@
+import XCTest
+@testable import TinyPublisher
+import Combine
+
+final class PublisherAssignTests: XCTestCase {
+    
+    func testAssignBool() {
+        
+        var cancellables: [TinyPublisher.AnyCancellable] = []
+        
+        let subject = TinyPublisher.PassthroughSubject<Bool, Never>()
+        
+        class Test {
+            
+            let expectation: XCTestExpectation
+            
+            var value: Bool = false {
+                didSet {
+                    expectation.fulfill()
+                    XCTAssertTrue(value)
+                }
+            }
+            
+            init(expectation: XCTestExpectation) {
+                self.expectation = expectation
+            }
+        }
+        
+        let test = Test(expectation: expectation(description: "expecting true"))
+        
+        subject.assign(to: \.value, on: test).store(in: &cancellables)
+
+        subject.send(true)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    @available(iOS 13.0, *)
+    func testCombineAssignBool() {
+        
+        var cancellables: [Combine.AnyCancellable] = []
+        
+        let subject = Combine.PassthroughSubject<Bool, Never>()
+        
+        class Test {
+            
+            let expectation: XCTestExpectation
+            
+            var value: Bool = false {
+                didSet {
+                    expectation.fulfill()
+                    XCTAssertTrue(value)
+                }
+            }
+            
+            init(expectation: XCTestExpectation) {
+                self.expectation = expectation
+            }
+        }
+        
+        let test = Test(expectation: expectation(description: "expecting true"))
+        
+        subject.assign(to: \.value, on: test).store(in: &cancellables)
+        
+        subject.send(true)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    static var allTests = [
+        ("testPassthroughSubjectBool", testAssignBool),
+    ]
+}
