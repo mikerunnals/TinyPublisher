3da0c42fef9d2dd529ac89f4f08bd082cd94db6f
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-08-06 07:50:46 -0400

    WIP

--- TinyPublisher/Sources/TinyPublisher/AnyPublisher.swift
+++ TinyPublisher/Sources/TinyPublisher/AnyPublisher.swift
@@ -1,9 +1,24 @@
 extension Publisher {
     func eraseToAnyPublisher() -> AnyPublisher<Self.Output, Self.Failure> {
-        return AnyPublisher()
+        return AnyPublisher(subscribeClosure: { self.subscribe($0) })
     }
 }
 
-@frozen public struct AnyPublisher<Output, Failure> where Failure : Error {
+@frozen public struct AnyPublisher<Output, Failure>: Publisher where Failure : Error {
     
+    let subscribeClosure: (_ S: Subscriber) -> () 
+        
+    public func subscribe<S>(_ subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+        subscribeClosure(subscriber)
+    }
+    
+}
+
+protocol SubscribeClosure {
+    associatedtype Input
+    associatedtype Failure: Error
+
+    typealias subscribe = (_ S: Subscriber) -> Void
+
+    func perform<S: Subscriber>(then handler: @escaping S) where S : Subscriber, Self.Failure == S.Failure, Self.Input == S.Input
 }
--- TinyPublisher/Tests/TinyPublisherTests/AnyPublisherTests.swift
+++ TinyPublisher/Tests/TinyPublisherTests/AnyPublisherTests.swift
@@ -3,10 +3,34 @@ import XCTest
 import Combine
 
 /// see https://developer.apple.com/documentation/combine/publisher/erasetoanypublisher()
-
-
 final class AnyPublisherTests: XCTestCase {
 
+    @available(iOS 13.0.0, *)
+    func testTinyAnyPublisherCannotBeDowncast() {
+        
+        class TypeWithSubject {
+            public let publisher: some TinyPublisher.Publisher = TinyPublisher.PassthroughSubject<Int,Never>()
+        }
+
+        class TypeWithErasedSubject {
+            public let publisher: some TinyPublisher.Publisher = TinyPublisher.PassthroughSubject<Int,Never>()
+                .eraseToAnyPublisher()
+        }
+
+        let nonErased = TypeWithSubject()
+        if let subject = nonErased.publisher as? TinyPublisher.PassthroughSubject<Int,Never> {
+            print("Successfully cast nonErased.publisher. \(subject)")
+        } else {
+            XCTFail()
+        }
+        
+        let erased = TypeWithErasedSubject()
+        if let subject = erased.publisher as? TinyPublisher.PassthroughSubject<Int,Never> {
+            print("Successfully cast erased.publisher. \(subject)")
+            XCTFail()
+        }
+    }
+    
     @available(iOS 13.0, *)
     func testCombineAnyPublisherCannotBeDowncast() {
         
