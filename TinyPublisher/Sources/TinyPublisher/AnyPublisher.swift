bde870af6d32106a602ef245e3ec1ccadd57657b
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-08-07 09:22:46 -0400

    WIP

--- TinyPublisher/Sources/TinyPublisher/AnyPublisher.swift
+++ TinyPublisher/Sources/TinyPublisher/AnyPublisher.swift
@@ -9,15 +9,53 @@ extension Publisher {
     public typealias Output = Output
     public typealias Failure = Failure
     
-    let erasedPublisher: Any
-    
+    typealias GenericSubscriber<Output, Failure> = Subscriber
+    typealias SubscribingHandler = (GenericSubscriber<Output, Failure>) -> Void
+    private let subscribingClosure: (SubscribingHandler) -> Void
+                
     init<P: Publisher>(_ publisher: P) where Self.Failure == P.Failure, Self.Output == P.Output {
-        self.erasedPublisher = publisher
+        self.publisher = publisher.subscribe
     }
         
     public func subscribe<S>(_ subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-        if let publisher = erasedPublisher as? Publisher {
-            
-        }
+    }
+}
+
+class AnySubscriber<Input, Failure>: Subscriber where Failure : Error {
+    public typealias Input = Input
+    public typealias Failure = Failure
+
+    typealias ReceiveSubscriptionHandler = (Subscription) -> Void
+
+    private let receiveClosure: (ReceiveSubscriptionHandler) -> Void
+
+    init<S: Subscriber>(subscriber: S) where Failure == S.Failure, Input == S.Input{
+        receiveClosure = subscriber.receive
+    }
+    
+    func receive(_ input: Input) -> Subscribers.Demand {
+        return nil!
+    }
+    
+    func receive() -> Subscribers.Demand
+    
+    func receive(subscription: Subscription)
+
+    func receive(completion: Subscribers.Completion<Failure>)
+
+}
+
+
+class AnyModelLoader<T>: ModelLoading {
+    typealias CompletionHandler = (Result<T>) -> Void
+
+    private let loadingClosure: (CompletionHandler) -> Void
+
+    init<L: ModelLoading>(loader: L) where L.Model == T {
+        loadingClosure = loader.load
+    }
+
+    func load(completionHandler: CompletionHandler) {
+        loadingClosure(completionHandler)
     }
 }
