6e431bec026db2f274e3e5b9d3eaa4b11b57cd07
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-08-08 13:03:05 -0400

    Add Publishers Sequence

--- TinyPublisher/Sources/TinyPublisher/Operators/Map.swift
+++ TinyPublisher/Sources/TinyPublisher/Operators/Map.swift
@@ -1,43 +1,45 @@
 extension Publisher {
     
-    func map<T>(_ transform: @escaping (Self.Output) -> T) -> Map<AnyPublisher<Self.Output, Self.Failure>, T> {
-        return Map<AnyPublisher<Self.Output, Self.Failure>, T>(upstream: self as! AnyPublisher<Self.Output, Self.Failure>, transform: transform)
+    func map<T>(_ transform: @escaping (Self.Output) -> T) -> Publishers.Map<Self, T> {
+        return Publishers.Map<Self, T>(upstream: self, transform: transform)
     }
 }
 
-public class Map<Upstream, Output> : Publisher where Upstream : Publisher {
+extension Publishers {
+    public class Map<Upstream, Output> : Publisher where Upstream : Publisher {
 
-    public typealias Failure = Upstream.Failure
-    public typealias Output = Output
+        public typealias Failure = Upstream.Failure
+        public typealias Output = Output
 
-    public let upstream: Upstream
+        public let upstream: Upstream
 
-    public let transform: (Upstream.Output) -> Output
-    
-    private var cancellables: [TinyPublisher.AnyCancellable] = []
+        public let transform: (Upstream.Output) -> Output
+        
+        private var cancellables: [TinyPublisher.AnyCancellable] = []
 
-    public init(upstream: Upstream, transform: @escaping (Upstream.Output) -> Output) {
-        self.upstream = upstream
-        self.transform = transform
-    }
-
-    public func subscribe<S>(_ subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-
-        let sub = ClosureSubscriber<Upstream.Output, Failure>(receiveCompletion: receiveCompletion(subscriber),
-                                                               receiveValue: receiveValue(subscriber))
-        upstream.subscribe(sub)
-        cancellables.append(sub.eraseToAnyCancellable())
-    }
-    
-    private func receiveCompletion<S>(_ subscriber: S) -> ((Subscribers.Completion<Failure>) -> Void)? where S : Subscriber, Failure == S.Failure, Output == S.Input {
-        return { completion in
-            subscriber.receive(completion: completion)
+        public init(upstream: Upstream, transform: @escaping (Upstream.Output) -> Output) {
+            self.upstream = upstream
+            self.transform = transform
         }
-    }
-    
-    private func receiveValue<S>(_ subscriber: S) -> ((Upstream.Output) -> Void) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-        return { input in
-            subscriber.receive(self.transform(input))
+
+        public func subscribe<S>(_ subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+
+            let sub = ClosureSubscriber<Upstream.Output, Failure>(receiveCompletion: receiveCompletion(subscriber),
+                                                                   receiveValue: receiveValue(subscriber))
+            upstream.subscribe(sub)
+            cancellables.append(sub.eraseToAnyCancellable())
+        }
+        
+        private func receiveCompletion<S>(_ subscriber: S) -> ((Subscribers.Completion<Failure>) -> Void)? where S : Subscriber, Failure == S.Failure, Output == S.Input {
+            return { completion in
+                subscriber.receive(completion: completion)
+            }
+        }
+        
+        private func receiveValue<S>(_ subscriber: S) -> ((Upstream.Output) -> Void) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+            return { input in
+                subscriber.receive(self.transform(input))
+            }
         }
     }
 }
--- TinyPublisher/Tests/TinyPublisherTests/Operators/Map.swift
+++ TinyPublisher/Tests/TinyPublisherTests/Operators/Map.swift
@@ -1,42 +1,10 @@
 import XCTest
 @testable import TinyPublisher
-import Combine
+//import Combine // TODO: figure out how to call both Tiny and Combine
 
 final class MapTests: XCTestCase {
     
-    func testTiny() {
-        
-        var s = ""
-        
-        let numbers = [5, 4, 3, 2, 1, 0]
-        let romanNumeralDict: [Int : String] =
-           [1:"I", 2:"II", 3:"III", 4:"IV", 5:"V"]
-        
-        let subject = TinyPublisher.PassthroughSubject<Int, Never>()
-        let publisher = subject.eraseToAnyPublisher()
-        
-        let e = expectation(description: "Expect last item is \"(unknown)\"")
-        _ = publisher
-            .map { romanNumeralDict[$0] ?? "(unknown)" }
-            .sink { romanNum in
-                s = s + "\(romanNum)" + " "
-                if romanNum == "(unknown)" {
-                    e.fulfill()
-                }
-        }
-        
-        numbers.forEach { subject.send($0) }
-        
-        waitForExpectations(timeout: 3)
-        
-        XCTAssertEqual("V IV III II I (unknown) ", s)
-
-        // Prints: "V IV III II I (unknown)"
-    }
-
-    
-    @available(iOS 13.0.0, *)
-    func testCombine() {
+    func testMapIntToRomanNumeral() {
         
         var s = ""
         
@@ -51,4 +19,23 @@ final class MapTests: XCTestCase {
 
         // Prints: "V IV III II I (unknown)"
     }
+
+
+// import Combine needed for this compatiblity test:
+//    @available(iOS 13.0.0, *)
+//    func testCombine() {
+//
+//        var s = ""
+//
+//        let numbers = [5, 4, 3, 2, 1, 0]
+//        let romanNumeralDict: [Int : String] =
+//           [1:"I", 2:"II", 3:"III", 4:"IV", 5:"V"]
+//        _ = numbers.publisher
+//            .map { romanNumeralDict[$0] ?? "(unknown)" }
+//            .sink { s = s + "\($0)" + " " }
+//
+//        XCTAssertEqual("V IV III II I (unknown) ", s)
+//
+//        // Prints: "V IV III II I (unknown)"
+//    }
 }
