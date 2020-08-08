e93c2e539456fbb1b5b864950c6d0cd0096064e1
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-08-08 13:11:55 -0400

    Add Missing Sequence.swift File

---
+++ TinyPublisher/Sources/TinyPublisher/Subjects/Sequence.swift
@@ -0,0 +1,33 @@
+
+extension Sequence {
+    var publisher: Publishers.Sequence<Self, Never> {
+        get {
+            return Publishers.Sequence<Self, Never>(self)
+        }
+    }
+}
+
+extension Publishers {
+    
+    public struct Sequence<Elements, Failure>: Publisher where Elements: Swift.Sequence, Failure : Error {
+
+        public typealias Output = Elements.Element
+        public typealias Failure = Failure
+        
+        private let elements: Elements
+        private let subject = TinyPublisher.PassthroughSubject<Elements.Element, Failure>()
+        
+        init(_ elements: Elements) {
+            self.elements = elements
+        }
+        
+        public func subscribe<S>(_ subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+            subject.subscribe(subscriber)
+            
+            // send Elements now?
+            elements.forEach {
+                subject.send($0)
+            }
+        }
+    }
+}
