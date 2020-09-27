ba70e396acbe5b7f0c8983697055369ade4218d9
Mike Runnals <mike.runnals@willowtreeapps.com>
2020-09-27 13:50:08 -0400

    Better Name for Catch Unit Test

--- TinyPublisher/Tests/TinyPublisherTests/Operators/CatchTests.swift
+++ TinyPublisher/Tests/TinyPublisherTests/Operators/CatchTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 #if true
-import Combine // TODO: figure out how to call both Tiny and Combine
+import Combine
 #else
 @testable import TinyPublisher
 #endif
@@ -9,7 +9,7 @@ import Combine // TODO: figure out how to call both Tiny and Combine
 final class CatchTests: XCTestCase {
     
     @available(iOS 13.0.0, *)
-    func testTiny() {
+    func testCatchCanMapErrorToJust() {
         struct SimpleError: Error {}
         let numbers = [5, 4, 3, 2, 1, 0, 9, 8, 7, 6]
         var result: Int? = nil
@@ -29,22 +29,4 @@ final class CatchTests: XCTestCase {
 
         XCTAssertEqual(-1, result)
     }
-
-//    @available(iOS 13.0.0, *)
-//    func testCombine() {
-//        struct SimpleError: Error {}
-//        let numbers = [5, 4, 3, 2, 1, 0, 9, 8, 7, 6]
-//        var result: Int? = nil
-//        _ = numbers.publisher
-//            .tryLast(where: {
-//                guard $0 != 0 else {throw SimpleError()}
-//                return true
-//            })
-//            .catch({ (error) in
-//                Just(-1)
-//            })
-//            .sink { result = $0 }
-//
-//        XCTAssertEqual(-1, result)
-//    }
 }
