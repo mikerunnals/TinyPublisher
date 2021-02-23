3d76f1a19acb20b98a07451ac451251a89a3f3e3
Mike Runnals <mike.runnals@willowtreeapps.com>
2021-02-23 03:30:40 -0500

    Old School Project Template Wizard School Apps

---
+++ Examples/Wizard Old School/Wizard Old School/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wizard Old School
+//
+//  Created by Mike Runnals on 2/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Examples/Wizard Old School/Wizard Old School/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Wizard Old School
+//
+//  Created by Mike Runnals on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+    }
+
+
+}
+
